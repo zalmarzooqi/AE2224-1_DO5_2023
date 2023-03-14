990512da4055f8c4f1198741aea541e4140b7e74
mchenzhang <mglcz@outlook.com>
2023-03-14 10:16:50 +0100

    Update threshold.py

--- threshold.py
+++ threshold.py
@@ -1,7 +1,26 @@
-import csv
+import pandas as pd
+import glob
+import os
 
-results = []
-with open("0003-0019.csv") as csvfile:
-    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
-    for row in reader: # each row is a list
-        results.append(row)
+#use the glob module to get a list of all CSV files in the folder.
+folder_path = 'C:/Users/mglcz/OneDrive - Delft University of Technology/Project Q3 BSc-2/AE2224-1_DO5_2023/Immersion Uninhibited/LocalAnalysis_Margin=004px_time=10800/1_LocalImgAnal_Num'
+folder_path = os.path.abspath(folder_path)
+csv_files = glob.glob(folder_path + '/*.csv')
+
+#read each CSV file and store its data in a Pandas DataFrame object.
+data_frames = []
+
+for file in csv_files:
+    df = pd.read_csv(file)
+    data_frames.append(df)
+    
+#convert each DataFrame to a NumPy array by using the values attribute.
+arrays = []
+
+for df in data_frames:
+    arr = df.values
+    arrays.append(arr)
+
+print(len(arrays))
+
+
