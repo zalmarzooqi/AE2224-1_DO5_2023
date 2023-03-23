99acbdd7d10e56d362006803072ceeafce16a97d
mchenzhang <mglcz@outlook.com>
2023-03-23 16:55:59 +0100

    3

--- positivechange.py
+++ positivechange.py
@@ -34,6 +34,7 @@ for i in range(arrays[0].shape[0]):
 matrixsize_sum = (len(arrays),arrays[0].shape[0])
 sum_cumm  = np.zeros(matrixsize_sum)
 sum = np.zeros(matrixsize_sum)
+sumpercent = np.zeros(matrixsize_sum)
 
 #changed matrix
 matrixsize = (arrays[0].shape[0]-1, arrays[0].shape[1]-2)
@@ -42,6 +43,7 @@ changearray = np.zeros(matrixsize)
 
 #for the 1567 timesteps in each file
 for i in range(len(arrays)):
+    totalpix = arrays[i][0, 3:arrays[i].shape[1]].sum()
     for row in range(arrays[i].shape[0]):
         if row < 1566:
             #substract the ith+1 - ith row
@@ -57,6 +59,7 @@ for i in range(len(arrays)):
         sumpositives = np.sum(positivearray[j])
         sum_cumm[i,j+1] = sumpositives_cumm
         sum[i,j+1] = sumpositives
+        sumpercent[i,j+1] = sumpositives / totalpix * 100
 
 file_list = []
 for file_name in os.listdir(folder_path):
@@ -92,6 +95,21 @@ for i in range(len(arrays)):
     plt.xlabel('time [s]')
     plt.ylabel('Number of changed pixels')
 plt.savefig('unin_delta_to_time_10800/change_all.png')
+plt.clf()
+
+for i in range(len(arrays)):
+    plt.plot(timesteps_unin, sumpercent[i])
+    plt.xlabel('time [s]')
+    plt.ylabel('Percentage of changed pixels [%]')
+    #plt.ylim(0,110)
+    plt.savefig(f'unin_delta_percent_to_time_10800/change_{file_list[i]}.png')
+    plt.clf()
+    
+for i in range(len(arrays)):
+    plt.plot(timesteps_unin, sumpercent[i])
+    plt.xlabel('time [s]')
+    plt.ylabel('Percentage of changed pixels [%]')
+plt.savefig('unin_delta_percent_to_time_10800/change_all.png')
 
 #np.set_printoptions(threshold=sys.maxsize) 
 
