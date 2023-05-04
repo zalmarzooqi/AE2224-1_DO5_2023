e299a0ff905a3ad5458bbb91abce25bdafdbc83f
Floris02 <f.b.benschop@student.tudelft.nl>
2023-05-04 15:03:24 +0200

    added some new density functions

--- corrosionrate.py
+++ corrosionrate.py
@@ -1,28 +1,33 @@
 from corrosiononsetcolumn  import *
 from scipy.optimize import curve_fit
 
-#def current_density(t, k1, k2):
-    #return k1 / np.sqrt(t) * 1 / np.sqrt(1 + k2*t)
-def current_density(t,b,alpha):
-    return b*np.exp(-alpha*t)
+def current_density_lin(t, k1, k2):
+    return k1 / np.sqrt(t) * 1 / np.sqrt(1 + k2*t)
+def current_density_exp(t,b,alpha, c):
+    return b*np.exp(-alpha*t)+c
+
+def current_density_ln(t, r, p):
+    return p * np.log(r*t)
 
 if timesteps_unin[0] == 0:
     timesteps_unin[0] = 0.00000000000001
 
 def fit_particle(row):
-    popt, pcov = curve_fit(current_density, timesteps_unin, row, p0=[1e-8, 1e-5])
-    return popt[0], popt[1] # Return k1 (in A/m*s**0.5) and k2    
+    popt, pcov = curve_fit(current_density_ln, timesteps_unin, row, p0=[0.5, 0.01])
+    return popt[0], popt[1] # Return k1 (in A/m*s**0.5) and k2
 
 # Apply the fit_particle function to each row of i_matrix
 results = np.apply_along_axis(fit_particle, 1, matrixpercentabs)
 
 # Extract the estimated values of k1 and k2 from the results array
-b = results[:, 0]
-alpha = results[:, 1]
+r = results[:, 0]
+p = results[:, 1]
+
 
 print(timesteps_unin)
-print('k1 values:', b)
-print('k2 values:', alpha)
+print('r values:', r)
+print('p values:', p)
+
 
 steps = [i for i in range(1, (len(arrays) + 1), 1)]
 # # steps = np.matrix(steps)
@@ -42,16 +47,31 @@ plt.savefig(f'unin_S_abs_corr_to_time_10800/plot_k2.png') #change particle type
 plt.plot(steps, k1_value)
 plt.plot(steps, k2_value)
 plt.show()'''
+'''
+verify_values = []
+
+alfa = np.matrix(alpha)
+beta = np.matrix(b)
+gamma = np.matrix(c)
+
+
+verify_value = current_density(timesteps_unin, beta[:,0], alfa[:,0], gamma[:,0])
+verify_value = verify_value.T
+plt.plot(timesteps_unin,verify_value)
+plt.show()
+'''
+
 
 verify_values = []
 
-a = np.matrix(k1_value)
-b = np.matrix(k2_value)
+a = np.matrix(r)
+b = np.matrix(p)
 
 
-verify_value = current_density(timesteps_unin, a[:,0], b[:,0])
+verify_value = current_density_ln(timesteps_unin, a[:,0], b[:,0])
 verify_value = verify_value.T
 plt.plot(timesteps_unin,verify_value)
+plt.ylim(0, 100)
 plt.show()
 
 """"
