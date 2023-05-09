5cfeac4e83ff320d2325da2b3418a9cb88507cd5
zalmarzooqi <zayedalmarzouqi@yahoo.com>
2023-05-09 17:21:28 +0200

    boom backward regression like a boss update

---
+++ backregressapp.py
@@ -0,0 +1,76 @@
+import Backward_regression 
+from sklearn.metrics import mean_squared_error
+
+# Basic modules
+import os
+import sys
+import time
+import shutil
+
+# Installed modules
+import numpy as np
+import scipy
+import pandas as pd
+import matplotlib.pyplot as plt
+import tkinter as tk
+import tkinter.font
+from tkinter.filedialog import askdirectory, askopenfilename
+
+# # Custom modules
+# from guis import *
+# from scripts import *
+
+
+
+# Empty lists for use in per type plotting (all particles of one type)
+type_matrix_abs = []
+type_matrix_rel = []
+type_timesteps = []
+
+# Iterate over the different particles/csv files
+file_path = r"Sorted\S-phase//0022-0074.csv"
+main_out_path = r""
+data = pd.read_csv(file_path, header=None)          # Read file
+total_pixels = data.iloc[0, 3:].sum()               # Get the total amount of pixels
+print(total_pixels)
+# Empty lists for use in per particle plotting
+file_matrix_abs = []
+file_matrix_rel = []
+file_timesteps = []
+
+# Iterate over the different columns to find the threshold column
+for i in range(3, data.shape[1]):
+    if data.iloc[0, i] > 0:
+        sig_col = i                                 # Stop looking in the columns when the
+        break                                       # one has been found
+
+    # Some files are weird and do not have any values past the "C" column in the csv
+    # Currently I just skip these files/particles, but we may need to take a better look at them
+    if i == data.shape[1]-1:
+        print(f"No pixels outside of col. C found in file)")
+        sig_col = "Stop"
+
+# Iterate over the different rows to find the number of pixels that have passed the COC
+for j in range(data.shape[0]):
+    corroded_pixels = data.iloc[j, 3:sig_col].sum()
+    corroded_percentage = corroded_pixels / total_pixels * 100
+    file_matrix_abs.append(corroded_percentage)
+    if j == 0:
+        file_matrix_rel.append(corroded_percentage)
+    else:
+        file_matrix_rel.append(corroded_percentage-file_matrix_abs[j-1])
+
+    # Find the timesteps
+    file_timesteps.append(data.iloc[j, 1])
+    if file_timesteps[-1] >= 74000:
+        break
+
+# Smoothing data
+window_size = 15
+weights = np.repeat(1.0, window_size) / window_size
+file_matrix_abs_smooth = np.convolve(file_matrix_abs, weights, 'valid')
+file_matrix_abs_smooth_list = file_matrix_abs_smooth.tolist()
+
+t_start, k1, y_inter1, model_listk1 = Backward_regression.slope_k1(file_matrix_abs_smooth,file_timesteps,file_matrix_abs_smooth_list)
+t_start, k2, y_inter2, model_listk2 = Backward_regression.slope_k2(file_matrix_abs_smooth,file_timesteps,file_matrix_abs_smooth_list)
+t_star, val_star = Backward_regression.intersect(k1,y_inter1,k2,y_inter2,file_matrix_abs_smooth_list,file_timesteps)
