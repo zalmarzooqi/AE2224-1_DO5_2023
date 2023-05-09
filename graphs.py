0b3ec64c648cb1fac86833e65b03ffdbbd99b308
VPathirana <127831994+VPathirana@users.noreply.github.com>
2023-05-09 17:06:12 +0200

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -17,7 +17,7 @@ for k1 in range (1,len(data1[0])):
     y1=column(data1,k1)
     for m in range(len(y1)):
         y1[m]=100*y1[m]
-    axis[0].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,x1[-1]))
+    axis[0].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,3000))
     axis[0].plot(x1,y1)
     axis[0].set_title("Reimmersion Uninhibited")
 with open('CSVImmersion_uninhibited.csv', newline='') as csvfile:
@@ -28,7 +28,7 @@ for k2 in range (1,len(data2[0])):
     y2=column(data2,k2)
     for m in range(len(y2)):
         y2[m]=100*y2[m]
-    axis[1].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,x2[-1]))
+    axis[1].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,4000))
     axis[1].plot(x2,y2)
     axis[1].set_title("Immersion Uninhibited")
 with open('CSVDelayed.csv', newline='') as csvfile:
@@ -39,7 +39,7 @@ for k3 in range (1,len(data3[0])):
     y3=column(data3,k3)
     for m in range(len(y3)):
         y3[m]=100*y3[m]
-    axis[2].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,x3[-1]))
+    axis[2].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,3000))
     axis[2].plot(x3,y3)
     axis[2].set_title("Delayed")
     #plt.plot(x,y)
