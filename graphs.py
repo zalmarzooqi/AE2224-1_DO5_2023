2ed0b2c440fd2e3dcecb7d5a437e57962094b3ce
VPathirana <127831994+VPathirana@users.noreply.github.com>
2023-05-09 17:00:31 +0200

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -8,17 +8,39 @@ def column(data, i):
     for j in range (1,(len(data))):
         column.append(float(data[j][i])) 
     return column
-
+figure, axis = plt.subplots(3)
 with open('CSVReimmersion_uninhibitedfiltered.csv', newline='') as csvfile:
-    data = list(csv.reader(csvfile))
-print(len(data))
-x=column(data,0)
-for k in range (1,len(data[0])):
-    y=column(data,k)
-    for m in range(len(y)):
-        y[m]=100*y[m]
-    plt.xlim(0,x[-1])
-    plt.xlabel("Time [s]")
-    plt.ylabel("Percentage of changed pixels [%]")
-    plt.plot(x,y)
+    data1 = list(csv.reader(csvfile))
+print(len(data1))
+x1=column(data1,0)
+for k1 in range (1,len(data1[0])):
+    y1=column(data1,k1)
+    for m in range(len(y1)):
+        y1[m]=100*y1[m]
+    axis[0].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,x1[-1]))
+    axis[0].plot(x1,y1)
+    axis[0].set_title("Reimmersion Uninhibited")
+with open('CSVImmersion_uninhibited.csv', newline='') as csvfile:
+    data2 = list(csv.reader(csvfile))
+print(len(data2))
+x2=column(data2,0)
+for k2 in range (1,len(data2[0])):
+    y2=column(data2,k2)
+    for m in range(len(y2)):
+        y2[m]=100*y2[m]
+    axis[0].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,x2[-1]))
+    axis[0].plot(x2,y2)
+    axis[0].set_title("Immersion Uninhibited")
+with open('CSVDelayed.csv', newline='') as csvfile:
+    data3 = list(csv.reader(csvfile))
+print(len(data3))
+x3=column(data3,0)
+for k3 in range (1,len(data3[0])):
+    y3=column(data3,k3)
+    for m in range(len(y3)):
+        y3[m]=100*y3[m]
+    axis[0].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,x3[-1]))
+    axis[0].plot(x3,y3)
+    axis[0].set_title("Delayed")
+    #plt.plot(x,y)
 plt.show()
