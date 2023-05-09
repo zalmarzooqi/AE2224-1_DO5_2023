c08e2000b7758cb6743cf5cd51970ae6633cd7c6
VPathirana <127831994+VPathirana@users.noreply.github.com>
2023-05-09 17:10:08 +0200

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -17,8 +17,11 @@ for k1 in range (1,len(data1[0])):
     y1=column(data1,k1)
     for m in range(len(y1)):
         y1[m]=100*y1[m]
+    kernel_size = 5
+    kernel = np.ones(kernel_size) / kernel_size
+    y1_convolved = np.convolve(y1, kernel, mode='same')
     axis[0].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,3000))
-    axis[0].plot(x1,y1)
+    axis[0].plot(x1,y1_convolved)
     axis[0].set_title("Reimmersion Uninhibited")
 with open('CSVImmersion_uninhibited.csv', newline='') as csvfile:
     data2 = list(csv.reader(csvfile))
@@ -28,8 +31,11 @@ for k2 in range (1,len(data2[0])):
     y2=column(data2,k2)
     for m in range(len(y2)):
         y2[m]=100*y2[m]
+    kernel_size = 5
+    kernel = np.ones(kernel_size) / kernel_size
+    y2_convolved = np.convolve(y2, kernel, mode='same')
     axis[1].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,4000))
-    axis[1].plot(x2,y2)
+    axis[1].plot(x2,y2_convolved)
     axis[1].set_title("Immersion Uninhibited")
 with open('CSVDelayed.csv', newline='') as csvfile:
     data3 = list(csv.reader(csvfile))
@@ -39,8 +45,11 @@ for k3 in range (1,len(data3[0])):
     y3=column(data3,k3)
     for m in range(len(y3)):
         y3[m]=100*y3[m]
+    kernel_size = 5
+    kernel = np.ones(kernel_size) / kernel_size
+    y3_convolved = np.convolve(y3, kernel, mode='same')
     axis[2].set(xlabel="Time [s]",ylabel="Percentage of changed pixels [%]",xlim=(0,3000))
-    axis[2].plot(x3,y3)
+    axis[2].plot(x3,y3_convolved)
     axis[2].set_title("Delayed")
     #plt.plot(x,y)
 plt.show()
