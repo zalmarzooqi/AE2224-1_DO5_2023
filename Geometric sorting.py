04b08613c689b6361f0d38aca331bb2ce9c70c52
Floris02 <f.b.benschop@student.tudelft.nl>
2023-05-09 15:28:33 +0200

    Made the output as a pretty list

--- Geometric sorting.py
+++ Geometric sorting.py
@@ -11,24 +11,31 @@ case_list = ['Sorted_ParticleGeomCompo_inhb_del.xlsx', 'Sorted_ParticleGeomCompo
 type_list = ['ROI', 'Area', 'Circ.', 'Feret', 'AR', 'Solidity']
 comp_list = ['S-phase', 'Theta', 'Secondary']
 
+
+value_list = []
 typelist = copy.deepcopy(type_list)
 
 for i in range(len(case_list)):
     path = os.path.join(*p, case_list[i])
-    print(path)
+    #print(path)
     data = pd.read_excel(path, sheet_name='Geometry Filtered')
+    value_list.append([])
     for l in range(len(comp_list)):
+        value_list[i].append([])
         data_secondary = data[data.Type == comp_list[l]]
-        print(comp_list[l])
+        #print(comp_list[l])
         for j in range(len(type_list)):
-            print(type_list[j])
+            #print(type_list[j])
             typelist[j] = data_secondary[type_list[j]].values.tolist()
             for k, val in enumerate(typelist[j]):
                 if val == 1000:
-                    typelist[j][k] = val/1000
+                    typelist[j][k] = 1
 
-            print(typelist[j])
+            #print(typelist[j])
+            value_list[i][l].append(typelist[j])
 
-    for j in range(len(type_list)):
+"""    for j in range(len(type_list)):
         typelist(case_list[i]).append(data_secondary[type_list[j]].values.tolist())
-        print(typelist[i][j])
+        print(typelist[i][j]) """
+
+print(value_list)
