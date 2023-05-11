b11c00d519f957f410c17cfede5619e0edea1cae
zalmarzooqi <zayedalmarzouqi@yahoo.com>
2023-05-11 13:48:57 +0200

    lol

--- backregressapp.py
+++ backregressapp.py
@@ -1,4 +1,4 @@
-import Backward_regression 
+
 from sklearn.metrics import mean_squared_error
 
 # Basic modules
@@ -20,7 +20,54 @@ from tkinter.filedialog import askdirectory, askopenfilename
 # from guis import *
 # from scripts import *
 
+def slope_k1(file_matrix_abs_smooth,file_timesteps,file_matrix_abs_smooth_list):
+# Find t_start and slope
+    margin  = 0.95
+    start_val = 5
+    for val in file_matrix_abs_smooth:
+        if val > start_val:
+            t_start = file_timesteps[file_matrix_abs_smooth_list.index(val)]
+            val_start = val
+            break
+    slope_val = 25
+    for val in file_matrix_abs_smooth:
+        if val>slope_val:
+            t_slope = file_timesteps[file_matrix_abs_smooth_list.index(val)]
+            val_slope = val
+            break
+    k1 = (val_slope - val_start) / (t_slope - t_start)
+    y_inter1 = val_slope-t_slope*k1
+    model_listk1 = [k1 * (i-t_start)+y_inter1 for i in file_timesteps ]
+    return t_start, k1, y_inter1, model_listk1
 
+def slope_k2(file_matrix_abs_smooth,file_timesteps,file_matrix_abs_smooth_list):
+#Find t100
+    margin  = 0.95
+    slope100_val = 100
+    for val in file_matrix_abs_smooth:
+        if val < slope100_val or val> margin*slope100_val:
+            t_100slope = file_timesteps[file_matrix_abs_smooth_list.index(val)]
+            val_100slope = val
+            break
+    slope80_val = 75
+    for val in file_matrix_abs_smooth:
+        if val > slope80_val:
+            t_80slope = file_timesteps[file_matrix_abs_smooth_list.index(val)]
+            val_80slope = val
+            break
+    k2 = (val_100slope-val_80slope)/(t_100slope-t_80slope)
+    y_inter2 = val_100slope-k2*t_100slope
+    model_listk2 = [k2*(i-t_80slope)+y_inter2 for i in file_timesteps]
+    return t_100slope, k2, y_inter2, model_listk2
+
+def intersect(k1,y_inter1,k2,y_inter2,file_matrix_abs_smooth_list,file_timesteps):
+    t_star = (y_inter2-y_inter1)/(k1-k2)
+    if t_star > 600:
+        rounded_t_star = round(t_star/10)*10
+    else:
+        rounded_t_star = round(t_star)
+    val_star = file_matrix_abs_smooth_list[file_timesteps.index(rounded_t_star)]
+    return t_star, val_star
 
 # Empty lists for use in per type plotting (all particles of one type)
 type_matrix_abs = []
@@ -71,6 +118,6 @@ weights = np.repeat(1.0, window_size) / window_size
 file_matrix_abs_smooth = np.convolve(file_matrix_abs, weights, 'valid')
 file_matrix_abs_smooth_list = file_matrix_abs_smooth.tolist()
 
-t_start, k1, y_inter1, model_listk1 = Backward_regression.slope_k1(file_matrix_abs_smooth,file_timesteps,file_matrix_abs_smooth_list)
-t_start, k2, y_inter2, model_listk2 = Backward_regression.slope_k2(file_matrix_abs_smooth,file_timesteps,file_matrix_abs_smooth_list)
-t_star, val_star = Backward_regression.intersect(k1,y_inter1,k2,y_inter2,file_matrix_abs_smooth_list,file_timesteps)
+t_start, k1, y_inter1, model_listk1 = slope_k1(file_matrix_abs_smooth,file_timesteps,file_matrix_abs_smooth_list)
+t_start, k2, y_inter2, model_listk2 = slope_k2(file_matrix_abs_smooth,file_timesteps,file_matrix_abs_smooth_list)
+t_star, val_star = intersect(k1,y_inter1,k2,y_inter2,file_matrix_abs_smooth_list,file_timesteps)
